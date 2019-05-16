b68ae15bca72e42c734cb1e58045c394fe51e121
Hamza Amer <hamer1563@gmail.com>
2019-05-16 17:48:50 -0400

    smallupdate

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,90 +1,17 @@
 'use strict';
 
-console.log('App.js is running');
-
-var app = {
-    title: 'Indecision App',
-    sub: 'put your life in the hands of a computer',
-    options: []
-};
-
-var onFormSubmit = function onFormSubmit(e) {
-    e.preventDefault();
-    var option = e.target.elements.option.value;
-
-    if (option) {
-        app.options.push(option);
-        e.target.elements.option.value = '';
-        render();
-    }
-};
-
-var reset = function reset() {
-    app.options = [];
-    render();
-};
-
-var onMakeDecision = function onMakeDecision() {
-    var randomNum = Math.floor(Math.random() * app.options.length);
-    var option = app.option[randomNum];
-};
-
-var appRoot = document.querySelector('#app');
-
-var numbers = [55, 101, 1000];
-
-var render = function render() {
-    var template = React.createElement(
+var toggle = function toggle() {
+    React.createElement(
         'div',
         null,
         React.createElement(
             'h1',
             null,
-            app.title
-        ),
-        app.sub && React.createElement(
-            'p',
-            null,
-            app.sub
-        ),
-        app.options.length > 0 ? React.createElement(
-            'p',
-            null,
-            'Here are your options'
-        ) : 'no options',
-        React.createElement(
-            'button',
-            { disabled: app.options.length === 0, onClick: onMakeDecision },
-            'What should I do?'
-        ),
-        React.createElement(
-            'button',
-            { onClick: reset },
-            'Clear'
-        ),
-        React.createElement(
-            'ol',
-            null,
-            app.options.map(function (opt) {
-                return React.createElement(
-                    'li',
-                    { key: opt },
-                    opt
-                );
-            })
-        ),
-        React.createElement(
-            'form',
-            { onSubmit: onFormSubmit },
-            React.createElement('input', { type: 'text', name: 'option' }),
-            React.createElement(
-                'button',
-                null,
-                'Add option'
-            )
+            'Hey there'
         )
     );
-    ReactDOM.render(template, appRoot);
 };
 
-render();
+var appRoot = document.querySelector('#app');
+
+ReactDom.render(toggle, appRoot);
---
+++ src/playground/toggle-app.js
@@ -0,0 +1,13 @@
+
+
+const toggle = ()=>{
+    <div>
+    <h1>Hey there</h1>
+    </div>
+}
+
+
+
+const appRoot = document.querySelector('#app');
+
+ReactDom.render(toggle,appRoot);
